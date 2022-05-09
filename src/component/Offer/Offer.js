cbe351ba3b63d1a64670ee22bdb7a23ebe66b72e
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-05-09 21:12:32 +0600

    In blog route 3 question ans

--- src/component/Offer/Offer.js
+++ src/component/Offer/Offer.js
@@ -3,7 +3,7 @@ import offer from '../../images/offer.jpg'
 const Offer = () => {
     return (
         <div className='text-center mt-5'>
-            <h1><span className='border bg-info px-2 '>Product Offer</span> </h1>
+            <h1 className='bg-warning'><span className='border px-2 '>Product Offer</span> </h1>
             <h4>All Product 50% off</h4>
            <img src={offer} alt="" /> 
         </div>
--- src/component/OtherProduct/OtherProduct.js
+++ src/component/OtherProduct/OtherProduct.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const OtherProduct = () => {
     return (
-        <div className='text-center mt-5 bg-info'>
-           <h1>Other Products</h1> 
-           <p>Here you will find all the computer parts</p>
-           <p>Keyboard</p>
-           <p>Mouse</p>
-           <p>Mouse pad</p>
-           <p>Monitor</p>
+        <div className='text-center mt-5 bg-black'>
+           <h1 className='text-white'>Other Products</h1> 
+           <p className='text-white'>Here you will find all the computer parts</p>
+           <p className='text-warning'>Keyboard</p>
+           <p className='text-warning'>Mouse</p>
+           <p className='text-warning'>Mouse pad</p>
+           <p className='text-warning'>Monitor</p>
         </div>
     );
 };
